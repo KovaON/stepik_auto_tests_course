59f817a45fd19646b688f0c9bacf7f89c7a478fb
KovaON <kov.anton94@gmail.com>
2022-01-19 12:20:59 +0500

    Второй коммит

---
+++ Selenium step2.1.5.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    case = browser.find_element_by_id('treasure')
+    x = case.get_attribute("valuex")
+    print(x)
+    x=calc(x)
+    answer = browser.find_element_by_id('answer')
+    answer.send_keys(x)
+
+    checkbox = browser.find_element_by_id('robotCheckbox')
+    checkbox.click()
+
+    radiobutton = browser.find_element_by_id('robotsRule')
+    radiobutton.click()
+
+    submit = browser.find_element_by_css_selector('button.btn-default')
+    submit.click()
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
